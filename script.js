c0bbbf8efdd10b9a27edf5e4e0fc57e6c25d5547
sree-lakshmi-n <sreelakshmin153@gmai.com>
2021-11-10 09:33:01 +0530

    Active Player display done.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //end game loosing move  done
 //declare result   
-//player turn initialise
+//player turn initialise   done
 //row button disable issue        done
 
 function nimGame(){										// Global variables
@@ -19,14 +19,14 @@ function $(selector){																// ID selector
 function _(selector){
 	return document.getElementsByClassName(selector);		// Class selector
 }
-function disableBtn(elem){
+function disableBtn(elem){														//Disables the given button
 	elem.classList.add("disable-pointer");
 }
-function enableBtn(elem){
+function enableBtn(elem){															// Enables the given button
 	elem.classList.remove("disable-pointer");
 }
-function displayActivePlayer(){
-	$('active-player').innerText = nim.getValue('activePlayer') + " won";
+function displayActivePlayer(){												//result
+	$('active-player').innerText = nim.getValue('activePlayer');
 }
 
 var nim = new nimGame();
@@ -40,7 +40,8 @@ function generateMatches(){															// Matches generated. The game area is
 		}
 	}
 }
-function hideMatch(rowNum) {
+/* Function to hide the matches corresponding to the row clicked */
+function hideMatch(rowNum) {														
 	let rowChildren = $("row"+rowNum).children;           // rowNum ranges from 1 to 4.
 	for(let i = 1; i <= rowChildren.length-1; i++){
 		if(!(rowChildren[i].classList.contains("hide"))){
@@ -49,11 +50,13 @@ function hideMatch(rowNum) {
 		}
 	}
 }
+/* Function to show the matches. Useful when a new game begins. */
 function showMatch(elem) {
 	for(let i=1; i<elem.children.length; i++){
 		elem.children[i].classList.remove("hide");
 	}
 }
+/* Other rows are to be disabled when a particular row is clicked so that human can hide matches only from the row he has clicked. */
 function disableOtherRows(rowNum){
 	let rows = _("row");
 	for(let i=0;i<nim.getValue('maxRowNum');i++){
@@ -62,12 +65,14 @@ function disableOtherRows(rowNum){
 		}
 	}
 }
+/* Function to enable the disabled rows. */
 function enableRows(){
 	let rows = _("row");
 	for(let i=0;i<nim.getValue('maxRowNum');i++){
 			rows[i].children[0].classList.remove("disable-pointer");
 		}
 }
+/* Function to calculate the no. of visible matches in a particular row. */
 function noOfMatchesLeft(rowNum){
 	let matchesLeft = 0;
 	for(let i=1; i<$("row"+rowNum).children.length; i++){
@@ -77,8 +82,10 @@ function noOfMatchesLeft(rowNum){
 	}
 	return matchesLeft;
 }
+/************************** Comp move *************************/
 function makeMove(){
-	// nim.setValue('activePlayer',"Comp");
+	nim.setValue('activePlayer',"Comp");
+	displayActivePlayer();
 	let rows = _("row");
 	let stacks = [0,0,0,0];
 	for(let i = 0; i < rows.length; i++){
@@ -98,50 +105,41 @@ function makeMove(){
 		}
 	}
 	else{
-		if(isEndGame){
-			nim.setValue('activePlayer',"Comp");
-			displayActivePlayer();
-		}
 		for(let i=0; i<optimal[1]; i++){
 			hideMatch(optimal[0]+1);
 		}
 	}
-	if(isEndGame && optimal==undefined){
-			nim.setValue('activePlayer',"Human");
-			displayActivePlayer();
-		}
-	// nim.setValue('activePlayer',"Human");
+	nim.setValue('activePlayer',"Human");
 	disableBtn($('pc-move-btn'));
 	enableRows();
 }
-function isEndGame(stacks){
-	var is_endgame = stacks.reduce((r, e) => r + (e > 1), 0) < 2; //counts the no. of rows and matches remaining. Alerts if it's the last move.
-	return is_endgame;
-}
 function optimal_move(stacks) {
   var stacks_xor = stacks.reduce((r, e) => r ^ e, 0);          // nimsum calculation
-  var is_endgame = isEndGame(stacks);
+	var is_endgame = stacks.reduce((r, e) => r + (e > 1), 0) < 2; //counts the no. of rows and matches remaining. Alerts if it's the last move.
   var move = stacks.reduce((move, stack, i) => {
     var take = stack - (is_endgame ^ stack ^ stacks_xor);
     return take > move[1] ? [i, take] : move;
   }, [0, 0]);
   return move[1] > 0 ? move : undefined;
 }
+/************************** Human move *************************/
 function humanMove(rowNum){
-	// displayActivePlayer();
-	// nim.setValue('activePlayer',"Human");
+	nim.setValue('activePlayer',"Human");
+	displayActivePlayer();
 	if(noOfMatchesLeft(rowNum)>0){
 		disableOtherRows(rowNum-1);
 		enableBtn($('pc-move-btn'));
 	}															
 	hideMatch(rowNum);
+	nim.setValue('activePlayer',"Comp");
 }
+/************************** New Game Reset *************************/
 function newGame(){
 	let rows = _("row");
 	for(let i = 0; i < rows.length; i++) {
 	  showMatch(rows[i]);
 	}
-	// nim.setValue('activePlayer',"Human");
+	nim.setValue('activePlayer',"Human");
 	enableBtn($('pc-move-btn'));
 	enableRows();
 }
